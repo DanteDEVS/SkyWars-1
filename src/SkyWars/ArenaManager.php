70bfdf13984af2b7357fad61bda08dd589cbfcf4
HenryDevMaster <100979680+Henry12960@users.noreply.github.com>
2022-06-07 18:58:57 -0300

    Update ArenaManager.php

--- src/SkyWars/ArenaManager.php
+++ src/SkyWars/ArenaManager.php
@@ -1,40 +1,14 @@
 <?php
-/**
- * Adapted from the Wizardry License
- *
- * Copyright (c) 2015-2019 larryTheCoder and contributors
- *
- * Permission is hereby granted to any persons and/or organizations
- * using this software to copy, modify, merge, publish, and distribute it.
- * Said persons and/or organizations are not allowed to use the software or
- * any derivatives of the work for commercial use or any other means to generate
- * income, nor are they allowed to claim this software as their own.
- *
- * The persons and/or organizations are also disallowed from sub-licensing
- * and/or trademarking this software without explicit permission from larryTheCoder.
- *
- * Any persons and/or organizations using this software must disclose their
- * source code and have it publicly available, include this license,
- * provide sufficient credit to the original authors of the project (IE: larryTheCoder),
- * as well as provide a link to the original project.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A PARTICULAR
- * PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
- * USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 
-namespace larryTheCoder;
+namespace SkyWars;
 
-use larryTheCoder\arena\api\impl\ArenaState;
-use larryTheCoder\arena\api\task\AsyncDirectoryDelete;
-use larryTheCoder\arena\ArenaImpl;
-use larryTheCoder\utils\ConfigManager;
-use larryTheCoder\utils\Utils;
-use larryTheCoder\worker\LevelAsyncPool;
-use pocketmine\Player;
+use SkyWars\arena\api\impl\ArenaState;
+use SkyWars\arena\api\task\AsyncDirectoryDelete;
+use SkyWars\arena\ArenaImpl;
+use SkyWars\utils\ConfigManager;
+use SkyWars\utils\Utils;
+use SkyWars\worker\LevelAsyncPool;
+use pocketmine\Player\Player;
 use pocketmine\utils\Config;
 
 final class ArenaManager {
