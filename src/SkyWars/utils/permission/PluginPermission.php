5b69421938576d0d7453c7b288d7d0d2579776b0
HenryDevMaster <100979680+Henry12960@users.noreply.github.com>
2022-06-09 08:18:54 -0300

    Update PluginPermission.php

--- src/SkyWars/utils/permission/PluginPermission.php
+++ src/SkyWars/utils/permission/PluginPermission.php
@@ -1,30 +1,4 @@
 <?php
-/*
- * Adapted from the Wizardry License
- *
- * Copyright (c) 2015-2020 larryTheCoder and contributors
- *
- * Permission is hereby granted to any persons and/or organizations
- * using this software to copy, modify, merge, publish, and distribute it.
- * Said persons and/or organizations are not allowed to use the software or
- * any derivatives of the work for commercial use or any other means to generate
- * income, nor are they allowed to claim this software as their own.
- *
- * The persons and/or organizations are also disallowed from sub-licensing
- * and/or trademarking this software without explicit permission from larryTheCoder.
- *
- * Any persons and/or organizations using this software must disclose their
- * source code and have it publicly available, include this license,
- * provide sufficient credit to the original authors of the project (IE: larryTheCoder),
- * as well as provide a link to the original project.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A PARTICULAR
- * PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
- * USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 
 declare(strict_types = 1);
 
@@ -32,7 +6,7 @@ namespace SkyWars\utils\permission;
 
 use SkyWars\arena\api\utils\SingletonTrait;
 use SkyWars\database\SkyWarsDatabase;
-use SkyWars\SkyWarsPE;
+use SkyWars\MCSkyWars;
 use SkyWars\utils\PlayerData;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
@@ -67,7 +41,7 @@ class PluginPermission implements Listener {
 	public function onPlayerJoinEvent(PlayerJoinEvent $event): void{
 		$player = $event->getPlayer();
 
-		$this->attachments[$player->getName()] = $player->addAttachment(SkyWarsPE::getInstance());
+		$this->attachments[$player->getName()] = $player->addAttachment(MCSkyWars::getInstance());
 
 		SkyWarsDatabase::getPlayerEntry($player, function(?PlayerData $result) use ($player){
 			if(!isset($this->attachments[$player->getName()]) || $result === null) return;
