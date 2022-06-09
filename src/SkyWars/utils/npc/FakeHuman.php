cf0499dd3eda1a6762bf7d02ba686f4c5ae81482
HenryDevMaster <100979680+Henry12960@users.noreply.github.com>
2022-06-09 08:20:40 -0300

    Update FakeHuman.php

--- src/SkyWars/utils/npc/FakeHuman.php
+++ src/SkyWars/utils/npc/FakeHuman.php
@@ -1,35 +1,9 @@
 <?php
-/**
- * Adapted from the Wizardry License
- *
- * Copyright (c) 2015-2018 larryTheCoder and contributors
- *
- * Permission is hereby granted to any persons and/or organizations
- * using this software to copy, modify, merge, publish, and distribute it.
- * Said persons and/or organizations are not allowed to use the software or
- * any derivatives of the work for commercial use or any other means to generate
- * income, nor are they allowed to claim this software as their own.
- *
- * The persons and/or organizations are also disallowed from sub-licensing
- * and/or trademarking this software without explicit permission from larryTheCoder.
- *
- * Any persons and/or organizations using this software must disclose their
- * source code and have it publicly available, include this license,
- * provide sufficient credit to the original authors of the project (IE: larryTheCoder),
- * as well as provide a link to the original project.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A PARTICULAR
- * PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
- * USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 
 namespace SkyWars\utils\npc;
 
 use SkyWars\arena\api\translation\TranslationContainer;
-use SkyWars\SkyWarsPE;
+use SkyWars\MCSkyWars;
 use pocketmine\entity\Human;
 use pocketmine\entity\Skin;
 use pocketmine\event\entity\EntityDamageEvent;
@@ -57,7 +31,7 @@ class FakeHuman extends Human {
 
 	public function __construct(Level $level, CompoundTag $nbt, int $pedestalLevel){
 		$nbtNew = new BigEndianNBTStream();
-		$compound = $nbtNew->readCompressed(@stream_get_contents(SkyWarsPE::getInstance()->getResource("metadata-fix.dat")));
+		$compound = $nbtNew->readCompressed(@stream_get_contents(MCSkyWars::getInstance()->getResource("metadata-fix.dat")));
 		if(!($compound instanceof CompoundTag)){
 			throw new \RuntimeException("Unable to read skin metadata from SkyWarsForPE resources folder, corrupted build?");
 		}
@@ -109,7 +83,7 @@ class FakeHuman extends Human {
 	}
 
 	private function fetchData(): void{
-		$pedestal = SkyWarsPE::getInstance()->getPedestals();
+		$pedestal = MCSkyWarss::getInstance()->getPedestals();
 		if($pedestal === null && !$this->isFlaggedForDespawn()){
 			$this->flagForDespawn();
 
