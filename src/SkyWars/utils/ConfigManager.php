4e7e060e2d34787d34523920511ca021c09ddfd0
HenryDevMaster <100979680+Henry12960@users.noreply.github.com>
2022-06-09 08:18:02 -0300

    Update ConfigManager.php

--- src/SkyWars/utils/ConfigManager.php
+++ src/SkyWars/utils/ConfigManager.php
@@ -1,30 +1,4 @@
 <?php
-/**
- * Adapted from the Wizardry License
- *
- * Copyright (c) 2015-2018 larryTheCoder and contributors
- *
- * Permission is hereby granted to any persons and/or organizations
- * using this software to copy, modify, merge, publish, and distribute it.
- * Said persons and/or organizations are not allowed to use the software or
- * any derivatives of the work for commercial use or any other means to generate
- * income, nor are they allowed to claim this software as their own.
- *
- * The persons and/or organizations are also disallowed from sub-licensing
- * and/or trademarking this software without explicit permission from larryTheCoder.
- *
- * Any persons and/or organizations using this software must disclose their
- * source code and have it publicly available, include this license,
- * provide sufficient credit to the original authors of the project (IE: larryTheCoder),
- * as well as provide a link to the original project.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A PARTICULAR
- * PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
- * USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 
 namespace SkyWars\utils;
 
@@ -34,7 +8,7 @@ use pocketmine\utils\Config;
  * Config manager class for arenas that
  * will be used to configure the arena
  *
- * @package larryTheCoder\utils
+ * @package SkyWars\utils
  */
 class ConfigManager {
 
