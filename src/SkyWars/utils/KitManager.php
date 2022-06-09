f469fea847bc596f6cb819759dbefff121bb5343
HenryDevMaster <100979680+Henry12960@users.noreply.github.com>
2022-06-09 08:16:47 -0300

    Update KitManager.php

--- src/SkyWars/utils/KitManager.php
+++ src/SkyWars/utils/KitManager.php
@@ -1,30 +1,4 @@
 <?php
-/*
- * Adapted from the Wizardry License
- *
- * Copyright (c) 2015-2020 larryTheCoder and contributors
- *
- * Permission is hereby granted to any persons and/or organizations
- * using this software to copy, modify, merge, publish, and distribute it.
- * Said persons and/or organizations are not allowed to use the software or
- * any derivatives of the work for commercial use or any other means to generate
- * income, nor are they allowed to claim this software as their own.
- *
- * The persons and/or organizations are also disallowed from sub-licensing
- * and/or trademarking this software without explicit permission from larryTheCoder.
- *
- * Any persons and/or organizations using this software must disclose their
- * source code and have it publicly available, include this license,
- * provide sufficient credit to the original authors of the project (IE: larryTheCoder),
- * as well as provide a link to the original project.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A PARTICULAR
- * PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
- * USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 
 declare(strict_types = 1);
 
@@ -45,13 +19,13 @@ use pocketmine\Server;
 use pocketmine\utils\TextFormat;
 
 /**
- * Kit Manager for SkyWarsForPE using EasyKits plugin.
+ * Kit Manager for MCSkyWars using EasyKits plugin.
  *
  * <p> EasyKits plugin has no proper documentation on how to perform specific
  * kits over a player in an arena, most of these functions are hacks to access the kits
  * over a plugin and as in cooldown and permission, those two has been stripped out from the code.
  *
- * @package larryTheCoder\utils
+ * @package SkyWars\utils
  */
 class KitManager implements Listener {
 
